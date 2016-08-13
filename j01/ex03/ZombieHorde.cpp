7ce0da8c06ca53ebfa4227736940e455663ab523
Vivien KLAOUSEN <vklaouse@e3r2p1.42.fr>
2016-08-14 01:42:17 +0200

    caraibes

--- j01/ex03/ZombieHorde.cpp
+++ j01/ex03/ZombieHorde.cpp
@@ -6,7 +6,7 @@
 //   By: vklaouse <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/08/14 00:52:46 by vklaouse          #+#    #+#             //
-//   Updated: 2016/08/14 01:34:16 by vklaouse         ###   ########.fr       //
+//   Updated: 2016/08/14 01:42:01 by vklaouse         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -17,13 +17,13 @@ ZombieHorde::ZombieHorde(int n)
 {
 	int rand;
 	int i = 0;
-	std::string randname[7] = {"William Turner","Elizabeth Swann","Pintel","Joshamee Gibbs","Ragetti", "Hector Barbossa", "Jack Sparrow"};
+	std::string randname[5] = {"William Turner","Elizabeth Swann","Pintel","Joshamee Gibbs","Ragetti"};
 	srand(time(NULL));
 	this->object = new Zombie[n];
 	this->N = n;
 	while (i < n)
 	{
-		rand = std::rand() % 7;
+		rand = std::rand() % 5;
 		this->object[i].type = "Pirate";
 		this->object[i].name = randname[rand];
 		i++;
