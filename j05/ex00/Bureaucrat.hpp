833742f883acb6d12c183503c04296d3c2449e50
Vivien KLAOUSEN <vklaouse@e3r2p3.42.fr>
2016-09-10 19:11:52 +0200

    youhou

--- j05/ex00/Bureaucrat.hpp
+++ j05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 //   By: vklaouse <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/09/08 18:06:23 by vklaouse          #+#    #+#             //
-//   Updated: 2016/09/08 18:35:59 by vklaouse         ###   ########.fr       //
+//   Updated: 2016/09/10 19:06:15 by vklaouse         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,6 +14,8 @@
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 class Bureaucrat {
 
@@ -25,22 +27,43 @@ private:
 public:
 
 	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
 	Bureaucrat(Bureaucrat const &src);
 	~Bureaucrat();
 
 	Bureaucrat &operator=(Bureaucrat const &src);
 
-	void getName();
-	void getGrade();
+	std::string getName() const;
+	int getGrade() const;
 	void increasesGrade();
 	void decreasesGrade();
 
-	class GradeTooHighException : public std::exception{
+	class GradeTooHighException : public std::exception {
+
+	public:
+		
+		GradeTooHighException();
+		GradeTooHighException(GradeTooHighException const &src);
+		~GradeTooHighException() throw();
+
+		GradeTooHighException &operator=(GradeTooHighException const &src);
+
+		virtual const char *what() const throw();
 
 	};
 
 	class GradeTooLowException : public std::exception {
-	
+
+	public:
+
+		GradeTooLowException();
+		GradeTooLowException(GradeTooLowException const &src);
+		~GradeTooLowException() throw();
+
+		GradeTooLowException &operator=(GradeTooLowException const &src);
+
+		virtual const char *what() const throw();
+
 	};
 
 };
--- j05/ex00/main.cpp
+++ j05/ex00/main.cpp
@@ -6,6 +6,23 @@
 //   By: vklaouse <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2016/09/08 18:06:36 by vklaouse          #+#    #+#             //
-//   Updated: 2016/09/08 18:06:37 by vklaouse         ###   ########.fr       //
+//   Updated: 2016/09/10 19:09:53 by vklaouse         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat j = Bureaucrat("Bob", 30);
+	Bureaucrat k = Bureaucrat("Giselle", 150);
+	Bureaucrat l = Bureaucrat("Conrad", 2);
+	std::cout << j << k << l;
+	j.increasesGrade();
+	k.increasesGrade();
+	std::cout << j << k << l;
+    k.decreasesGrade();
+	k.decreasesGrade();
+	std::cout << j << k << l;
+	return (0);
+}
